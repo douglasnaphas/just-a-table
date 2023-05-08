ca79706e14da44c2825a26cf4604bd8deedf2eaa
Douglas Naphas <douglasnaphas@gmail.com>
2023-05-08 17:53:33 -0400

    Create the provider in the stack
    gh-1
    
    Apparently I had forgotten one of the fundamental principles of CDK:
    instantiate with "this" as the first arg.

--- oidc/lib/oidc-stack.ts
+++ oidc/lib/oidc-stack.ts
@@ -10,8 +10,8 @@ import * as iam from "aws-cdk-lib/aws-iam";
 export class OidcStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-    const provider = new GithubActionsIdentityProvider(scope, "GithubProvider");
-    const deployRole = new GithubActionsRole(scope, "DeployRole", {
+    const provider = new GithubActionsIdentityProvider(this, "GithubProvider");
+    const deployRole = new GithubActionsRole(this, "DeployRole", {
       provider: provider,
       owner: process.env.GITHUB_REPOSITORY_OWNER || "",
       repo: process.env.GITHUB_REPOSITORY?.split("/")[1] || "",
