93fe590bca587ae0447ace88ff1d1e65805f6dde
Douglas Naphas <douglasnaphas@gmail.com>
2023-05-08 17:39:44 -0400

    Format lib/oidc-stack.ts

--- oidc/lib/oidc-stack.ts
+++ oidc/lib/oidc-stack.ts
@@ -1,18 +1,23 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-import { GithubActionsIdentityProvider, GithubActionsRole } from 'aws-cdk-github-oidc';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import {
+  GithubActionsIdentityProvider,
+  GithubActionsRole,
+} from "aws-cdk-github-oidc";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
-import * as iam from 'aws-cdk-lib/aws-iam'
+import * as iam from "aws-cdk-lib/aws-iam";
 
 export class OidcStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-    const provider = new GithubActionsIdentityProvider(scope, 'GithubProvider');
-    const deployRole = new GithubActionsRole(scope, 'DeployRole', {
+    const provider = new GithubActionsIdentityProvider(scope, "GithubProvider");
+    const deployRole = new GithubActionsRole(scope, "DeployRole", {
       provider: provider,
       owner: process.env.GITHUB_REPOSITORY_OWNER || "",
       repo: process.env.GITHUB_REPOSITORY?.split("/")[1] || "",
     });
-    deployRole.addManagedPolicy(iam.ManagedPolicy.fromAwsManagedPolicyName('AdministratorAccess'));
+    deployRole.addManagedPolicy(
+      iam.ManagedPolicy.fromAwsManagedPolicyName("AdministratorAccess")
+    );
   }
 }
