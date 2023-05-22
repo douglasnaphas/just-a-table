d370f646990b7ca9b75f31e5d6aca30190005933
Douglas Naphas <douglasnaphas@gmail.com>
2023-05-21 22:24:01 -0400

    Test rolename in CI
    gh-1

--- rolename/itest/itest.sh
+++ rolename/itest/itest.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-ACTUAL1=$(npx ../ --repo-owner douglasnaphas --repo-name just-a-table --prefix 'GitHubDeployer-' --hash-length 6)
+ACTUAL1=$(npx . --repo-owner douglasnaphas --repo-name just-a-table --prefix 'GitHubDeployer-' --hash-length 6)
 EXPECTED1=GitHubDeployer-73ac48
 if [[ "${ACTUAL1}" != "${EXPECTED1}" ]]
 then
