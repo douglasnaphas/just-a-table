cede1abd7dddba181c8ada0e76ac36f642f3c8e0
Douglas Naphas <douglasnaphas@gmail.com>
2023-05-22 00:51:04 -0400

    Note TODO in bin
    gh-1

--- oidc/bin/oidc.ts
+++ oidc/bin/oidc.ts
@@ -8,6 +8,7 @@ if (!process.env.DEPLOY_ROLE_NAME) {
   console.error("DEPLOY_ROLE_NAME must be specified");
   process.exit(1);
 }
+// figure out if there is already an OIDC provider in this account
 new OidcStack(app, "OidcStack", {
   deployRoleName: process.env.DEPLOY_ROLE_NAME,
 });
